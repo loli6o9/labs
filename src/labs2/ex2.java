c6aebe9ce5cbb2ad4024fadcf7717db952df87f6
loli6o9 <maksust@bk.ru>
2022-12-03 19:16:52 +0500

    Лаба 2

---
+++ src/labs2/ex2.java
@@ -0,0 +1,19 @@
+package labs2;
+
+import java.util.Scanner;
+
+public class ex2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число:");
+        int num = in.nextInt();
+
+        if(num%7==1 && num%5==2)
+        {
+            System.out.println("Удовлетворяет");
+        }
+        else {
+            System.out.println("Не делиться на 7 и 5 с остатками 1 и 2");
+        }
+        in.close();    }
+}
---
+++ src/labs2/ex3.java
@@ -0,0 +1,19 @@
+package labs2;
+
+import java.util.Scanner;
+
+public class ex3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число:");
+        int num = in.nextInt();
+
+        if(num%4==0 && num>10)
+        {
+            System.out.println("Удовлетворяет");
+        }
+        else {
+            System.out.println("Не удовлетворяет");
+        }
+        in.close();    }
+}
